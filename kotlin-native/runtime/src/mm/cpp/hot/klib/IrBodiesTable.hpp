16a5d8b5c66436d13ad5066bd90bb935d815f49f
Gabriele Pappalardo <gabriele.pappalardo@jetbrains.com>
2025-09-26 23:43:13 +0200

    add first version of klib deserializer

---
+++ kotlin-native/runtime/src/mm/cpp/hot/klib/IrBodiesTable.hpp
@@ -0,0 +1,15 @@
+//
+// Created by Gabriele.Pappalardo on 01/08/2025.
+//
+
+#ifndef IRBODIESTABLE_HPP
+#define IRBODIESTABLE_HPP
+
+#include "IrLazyArrayReader.hpp"
+
+namespace kotlin::ir {
+class IrBodiesTable : public IrLazyArrayReader {};
+}
+
+#endif //IRBODIESTABLE_HPP
+
